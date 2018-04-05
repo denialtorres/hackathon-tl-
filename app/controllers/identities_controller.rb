5ad2e86f9eb92fb29625ef22040a70a353849a2b
ponentesincausa <ponentesincausa@gmail.com>
2018-04-05 15:21:52 -0500

    views and crud for identity

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -1,24 +1,37 @@
 class IdentitiesController < ApplicationController
   def index
   end
-  
+
   def show
     @identity = Identity.find(params[:id])
   end
-  
+
+  def new
+    @identity = Identity.new
+  end
+
+  def create
+    @identity = Identity.new(identity_params)
+    if @identity.save
+      redirect_to @identity, notice: 'Persona registrada con éxito.'
+    else
+      render :new
+    end
+  end
+
   def edit
     @identity = Identity.find(params[:id])
   end
-  
+
   def update
     @identity = Identity.find(params[:id])
     if @identity.update(identity_params)
      redirect_to @identity
     else
-     render 'edit'
+     render :edit
     end
   end
-  
+
   private
   def identity_params
     params.require(:identity).permit!
